e168aaee302057384bdeb09aa14317394f83d8e1
JayHam1227 <jhamilton3@buffs.wtamu.edu>
2026-02-05 23:15:02 -0600

    Week 4's Practice code

---
+++ Practice4.cs
@@ -0,0 +1,39 @@
+﻿namespace Practice4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Give me an integer: ");
+        string input = Console.ReadLine();
+
+        string result = odd_or_even(input);
+        Console.WriteLine($"The result is: {result}");
+
+
+    }
+
+    static string odd_or_even(string num)
+    {
+        int number = Convert.ToInt32(num);
+        if (number % 2 == 0)
+        {
+            string result = "even";
+            return result;
+        }
+        else
+        {
+            string result = "odd";
+            return result;
+        }
+    }
+    
+
+}
+
+/* use "dotnet new console -o [wanted name] --use-program-main true" to create new projects
+
+  use "cd [folder name]" to enter the project folder
+
+  use "dotnet run" to run the project */
