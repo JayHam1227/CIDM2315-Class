29a905e31ddfecc1f35e94b34f8d43d261a7b11b
JayHam1227 <jhamilton3@buffs.wtamu.edu>
2026-02-16 14:51:33 -0600

    Week 5's practice

---
+++ Practice5.cs
@@ -0,0 +1,22 @@
+﻿namespace Practice5;
+
+class Program 
+{
+    //Excercise 1
+    static void Main(string[] args)
+    {
+        double CallThree = ThreeNumAvg(a: 1.5, b: 5.5, c: 6.5);
+        Console.WriteLine("Average of three given Values: " + CallThree);
+
+        double CallTwo = ThreeNumAvg(a: 1.5, c: 5.5);
+        Console.WriteLine("Average of two given Values: " + CallTwo);
+    }
+
+    static double ThreeNumAvg(double a, double c, double b = 3.5)
+    {
+        double avg = (a + b + c) / 3;
+
+        return avg;
+    }
+
+}
