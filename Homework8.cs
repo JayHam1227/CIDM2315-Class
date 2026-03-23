3ad58476fa991313cc004976aa4f48523374dae8
JayHam1227 <jhamilton3@buffs.wtamu.edu>
2026-03-23 15:34:25 -0500

    Add files via upload

---
+++ Homework8.cs
@@ -0,0 +1,91 @@
+﻿namespace Homework8;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Customer cus1 = new Customer(customerName :"Alice", customerAge:33, customerCity:"Amarillo", customerCredit:198.5);
+        Customer cus2 = new Customer(customerName :"Bob", customerAge:23, customerCity:"Amarillo", customerCredit:226);
+        Customer cus3 = new Customer(customerName :"Cathy", customerAge:45, customerCity:"Amarillo", customerCredit:89.0);
+        Customer cus4 = new Customer(customerName :"David", customerAge:58, customerCity:"Amarillo", customerCredit:198.5);
+        Customer cus5 = new Customer(customerName :"Jack", customerAge:28, customerCity:"Canyon", customerCredit:561.6);
+        Customer cus6 = new Customer(customerName :"Tom", customerAge:36, customerCity:"Canyon", customerCredit:98.4);
+        Customer cus7 = new Customer(customerName :"Tony", customerAge:24, customerCity:"Canyon", customerCredit:18.5);
+        Customer cus8 = new Customer(customerName :"Sam", customerAge:35, customerCity:"Canyon", customerCredit:228.3);
+
+
+ 	        Customer[] customer_list = {cus1, cus2, cus3, cus4, cus5, cus6, cus7, cus8};
+
+
+        // call Q1 method
+        TotalCredits(customer_list);
+
+        // call Q2 method
+        AmarilloAverageAge(customer_list);
+
+        // call Q3 method
+        CanyonAge(customer_list);
+    }
+
+    public static void TotalCredits(Customer[] customer_list)
+    {
+        double total_credit = 0;
+        foreach (Customer cus in customer_list)
+        {
+            total_credit += cus.customerCredit;
+        }
+        Console.WriteLine("Q1: The total credits are: {0}", total_credit);
+    }
+
+    public static void AmarilloAverageAge(Customer[] customer_list)
+    {
+        int total_age = 0;
+        int count = 0;
+        foreach (Customer cus in customer_list)
+        {
+            if (cus.customerCity == "Amarillo")
+            {
+                total_age += cus.customerAge;
+                count++;
+            }
+        }
+        double average_age = (double)total_age / count;
+        Console.WriteLine("Q2: The average age of customers in Amarillo is: {0}", average_age);
+    }
+
+    public static void CanyonAge(Customer[] customer_list)
+    {
+        Console.Write("Q3: Customers who live in Canyon and are over 30 years old:");
+        foreach (Customer cus in customer_list)
+        {
+            if (cus.customerCity == "Canyon" && cus.customerAge > 30)
+            {
+                Console.Write(" {0}, ", cus.customerName);
+            }
+        }
+    }
+
+}    
+
+class Customer
+{
+    public string customerName { get; set; } = string.Empty;
+    public int customerAge { get; set; } = 0;
+    public string customerCity { get; set; } = string.Empty;
+    public double customerCredit { get; set; } = 0.0;
+
+    public Customer(string customerName, int customerAge, string customerCity, double customerCredit)
+    {
+        this.customerName = customerName;
+        this.customerAge = customerAge;
+        this.customerCity = customerCity;
+        this.customerCredit = customerCredit;
+    }
+}
+
+
+/* use "dotnet new console -o [wanted name] --use-program-main true" to create new projects
+
+  use "cd [folder name]" to enter the project folder
+
+  use "dotnet run" to run the project */
---
+++ Practice8.cs
@@ -0,0 +1,117 @@
+﻿namespace Practice8;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Exercise 1
+        string[] name_array = new string[5] { "Alice", "Bob", "Cathy", "Tom", "Jack" };
+
+        Console.WriteLine(
+            "the second name is: {0}, the fourth name is: {1}.", 
+            name_array[1], name_array[3]
+        );
+
+        name_array[4] = "David";
+
+        foreach (string name in name_array)
+        {
+            Console.Write(name + " ");
+        }
+
+        Console.WriteLine();
+
+        //Exercise 2
+        int[,] arr_2d =
+        {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+        Array(arr_2d);
+
+        //Exercise 3
+        Food food1 = new Food("Juice", 3.49, "Drink");
+        Food food2 = new Food("Orange", 0.99, "Fruit");
+        Food food3 = new Food("Chicken", 8.99, "Meat");
+        Food food4 = new Food("Broccoli", 2.49, "Vegetable");
+        Food food5 = new Food("Banana", 0.59, "Fruit");
+        Food food6 = new Food("Beef", 14.99, "Meat");
+        Food food7 = new Food("Carrot", 1.29, "Vegetable");
+        Food food8 = new Food("Soda", 2.99, "Drink");
+
+        Food[] shopping_list = { food1, food2, food3, food4, food5, food6, food7, food8 };
+
+        TotalPrice(shopping_list);
+        TotalFruitPrice(shopping_list);
+        AveragePrice(shopping_list);
+    }
+
+    static void Array(int[,] arr_2d)
+    {
+        for (int row = 0; row < 3; row++)
+        {
+            for (int col = 0; col <= 2; col++)
+            {
+                if(arr_2d[row, col] % 2 == 0)
+                {
+                    Console.Write(arr_2d[row, col] + " ");
+                }
+            }
+        }
+    }
+
+    static void TotalPrice(Food[] shopping_list)
+    {
+        double total_price = 0.0;
+        foreach (Food food in shopping_list)
+        {
+            total_price += food.foodPrice;
+        }
+        Console.WriteLine("The total price of all food items is: {0}.", total_price);
+    }
+
+    static void TotalFruitPrice(Food[] shopping_list)
+    {
+        double total_fruit_price = 0.0;
+        foreach (Food food in shopping_list)
+        {
+            if (food.foodType == "Fruit")
+            {
+                total_fruit_price += food.foodPrice;
+            }
+        }
+        Console.WriteLine("The total price of all fruit items is: {0}.", total_fruit_price);
+    }
+
+    static void AveragePrice(Food[] shopping_list)
+    {
+        double total_price = 0.0;
+        foreach (Food food in shopping_list)
+        {
+            total_price += food.foodPrice;
+        }
+        double average_price = total_price / shopping_list.Length;
+        Console.WriteLine("The average price of all food items is: {0}.", average_price);
+    }
+}
+
+class Food
+{
+    public string foodName { get; set; } = string.Empty;
+    public double foodPrice { get; set; } = 0.0;
+    public string foodType { get; set; } = string.Empty;
+
+    public Food(string name, double price, string type)
+    {
+        this.foodName = name;
+        this.foodPrice = price;
+        this.foodType = type;
+    }
+}
+
+/* use "dotnet new console -o [wanted name] --use-program-main true" to create new projects
+
+  use "cd [folder name]" to enter the project folder
+
+  use "dotnet run" to run the project */
